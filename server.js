6c1e387cb7a156fea3cb0e7537728b54d13e3d2c
LAPTOP-31KR88FU\Gaurav <gaurav.cdac16@gmail.com>
2017-09-10 08:46:36 +0530

    other dependancies
    a

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
+var morgan = require("morgan");
 
 var app = express();
+
+app.use(morgan('dev'));
 
 app.listen(3000,function(error){
  
  if(error) throw error;
 	console.log("Server is running on 3000 port");
 
+})
+
+app.get('/',function(req,res){
+	var name = "Shree Swami Samartha";
+	res.json("My Guru Is :- " + name);
 })
