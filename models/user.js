e16de511b322578ded9bd5cdde66b798ea818251
LAPTOP-31KR88FU\Gaurav <gaurav.cdac16@gmail.com>
2017-09-10 08:54:31 +0530

    user model
    user scema + bcryt pass hash

---
+++ models/user.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+
+  email: { type: String, unique: true, lowercase: true},
+  password: String,
+
+  profile: {
+    name: { type: String, default: ''},
+    picture: { type: String, default: ''}
+  },
+
+  address: String,
+  history: [{
+    paid: { type: Number, default: 0},
+    item: { type: Schema.Types.ObjectId, ref: 'Product'}
+  }]
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
