f31681a390d26256d901792419370642676fd103
LAPTOP-31KR88FU\Gaurav <gaurav.cdac16@gmail.com>
2017-09-10 09:13:21 +0530

    mlab setup
    connected to db

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require("express");
 var morgan = require("morgan");
+var mongoose = require('mongoose'); 
 
 var app = express();
+
+//mlab gauravtalele1994 gauravtalele2015@gmail.com gauravtalele*123
+
+
+mongoose.connect('mongodb://root:sss*123@ds133044.mlab.com:33044/ecommerece', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
 
 app.use(morgan('dev'));
 
